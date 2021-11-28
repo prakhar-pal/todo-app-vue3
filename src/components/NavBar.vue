737d804fc8a7bdc4411b162cfaca89e03e8da3a1
Prakhar <prakharpalpfdr+github@gmail.com>
2021-11-28 16:53:37 +0530

    feat: highlight active link in nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,25 @@
 <template>
     <nav>
         <ul class="flex">
-            <li v-for="(link, index) in navLinks" :class="index != 0 ? 'mx-2': ''" class="nav-link">
+            <li
+                v-for="(link, index) in navLinks"
+                :class="getClassForLinkItem(link, index)"
+                class="nav-link">
                 <router-link :to="link.path">{{ link.title}}</router-link>
             </li>
         </ul>
         <hr class="border-t-8 border-gray-600"/>
     </nav>
 </template>
+
+<style scoped>
+    li.nav-link {
+        border-bottom: 2px solid transparent;
+    }
+    li.active {
+        border-bottom-color: teal;
+    }
+</style>
 
 <script>
     let navLinks = [
@@ -24,8 +36,18 @@
         data() {
             return { navLinks };
         },
-        updated() {
-            console.log(this.$router);
+        computed: {
+            currentPath() {
+                return this.$route.path;
+            }
+        },
+        methods: {
+            getClassForLinkItem(link, index){
+                let cls = '';
+                if(index != 0) cls+= ' mx-2';
+                if(link.path == this.currentPath) cls+= ' active';
+                return cls;
+            }
         }
     }
 </script>
